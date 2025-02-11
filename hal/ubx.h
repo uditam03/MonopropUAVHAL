67e2c919024e09961b562b8150cc9023e5bee3fc
uditam03 <mail4uditam@gmail.com>
2025-02-10 19:47:46 -0500

    Update ubx.h

--- hal/ubx.h
+++ hal/ubx.h
@@ -1,7 +1,7 @@
 #ifndef UBX_H
 #define UBX_H
 
-#include <HardwareSerial.h>
+#include <HardwareSerial.h> //Not added in the repository currently
 
 #define GPS_RX_PIN 8   // RX2 Pin for GPS (Teensy Pin 8)
 #define GPS_TX_PIN 7   // TX2 Pin for GPS (Teensy Pin 7)
